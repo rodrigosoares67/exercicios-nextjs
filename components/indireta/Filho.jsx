17f41fbc74ecdf346b7e45395f6804c37e01f622
Rodrigo de Oliveira Carvalho Soares <rodrigo.soares@expocaccer.com.br>
2022-06-11 10:13:41 -0300

    FEAT: Exercícios de comunicação de componentes

---
+++ components/indireta/Filho.jsx
@@ -0,0 +1,11 @@
+export default function Filho(props){
+  // "Passei no ENEM!"
+  console.log(props.funcao)
+  return (
+    <div>
+      <h1>Filho</h1>
+      <button onClick={props.funcao}>Falar com o pai #01</button>
+      <button onClick={() => props.funcao("Passei no ENEM!")}>Falar com o pai #02</button>
+    </div>
+  )
+}

e423a575509c3cbeb87d4c30910b9d9e2198d0eb
Rodrigo de Oliveira Carvalho Soares <rodrigo.soares@expocaccer.com.br>
2022-06-11 11:25:03 -0300

    FEAT: Função para gerar números megasena

---
+++ functions/mega.js
@@ -0,0 +1,19 @@
+function mega(qtde = 6, numeros = []){
+  if(qtde < 6 && qtde > 60){
+    throw "Quantidade inválida!"
+  }
+
+  if(numeros.length === qtde){
+    return numeros.sort()
+  }
+
+  const numeroAleatorio = parseInt(Math.random() * 60) + 1
+
+  if(!numeros.includes(numeroAleatorio)){
+    return mega(qtde, [...numeros, numeroAleatorio])
+  } else {
+    return mega(qtde, numeros)
+  }
+}
+
+console.log(mega())

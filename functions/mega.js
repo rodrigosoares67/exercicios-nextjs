fc3e1530ce50dac27f251adbe774a65aabeba0f6
Rodrigo de Oliveira Carvalho Soares <rodrigo.soares@expocaccer.com.br>
2022-06-11 11:26:03 -0300

    FEAT: Ajuste na ordenação dos números da megasena

--- functions/mega.js
+++ functions/mega.js
@@ -4,7 +4,7 @@ function mega(qtde = 6, numeros = []){
   }
 
   if(numeros.length === qtde){
-    return numeros.sort()
+    return numeros.sort((n1, n2) => n1 - n2)
   }
 
   const numeroAleatorio = parseInt(Math.random() * 60) + 1
